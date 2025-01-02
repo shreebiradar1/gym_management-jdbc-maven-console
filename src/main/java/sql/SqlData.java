77a338a0c16c7331ca0013634ab1010cac112cf5
shree biradar <158836387+shreebiradar1@users.noreply.github.com>
2025-01-02 17:52:34 +0530

    Update SqlData.java

--- src/main/java/sql/SqlData.java
+++ src/main/java/sql/SqlData.java
@@ -1,8 +1,11 @@
 package sql;
 
 public class SqlData {
+	//if you want to connect copy your schema URL and paste it here					
 	protected final static String url = "jdbc:mysql://127.0.0.1:3306/gym_management";
-	protected final static String username = "root";
-	protected final static String password = "Shree@123";
+
+	// Assign your username and password
+	protected final static String username = "****";	//Username and password is hidden for security purpose
+	protected final static String password = "*********";
 	
 }
